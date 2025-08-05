8efa71e0f3391062c2b124f400ba8e70a0a19860
Alexandr Moroz <afmoroz@gmail.com>
2025-08-05 05:02:04 +0000

    feat: update graceful

--- pkg/graceful/graceful.go
+++ pkg/graceful/graceful.go
@@ -3,6 +3,7 @@ package graceful
 import (
 	"context"
 	"fmt"
+	"io"
 	"log/slog"
 	"os"
 	"os/signal"
@@ -11,19 +12,46 @@ import (
 	"time"
 )
 
+var logger = slog.Default()
+
+// SetLogger allows setting a custom logger for graceful shutdown operations,
+// or disables logging by using io.Discard.
+func SetLogger(l *slog.Logger) {
+	if l == nil {
+		logger = slog.New(slog.NewTextHandler(io.Discard, nil))
+	} else {
+		logger = l
+	}
+}
+
+// Operation defines a function type for cleanup operations that can be executed
+// during the shutdown process. It takes a context and returns an error if any.
 type Operation = func(ctx context.Context) error
 
-func Shutdown(ctx context.Context, timeout time.Duration, ops map[string]Operation) <-chan struct{} {
+// Shutdown gracefully shuts down the application by executing cleanup operations
+// defined in the ops map. It listens for specified OS signals and waits for
+// the context to be done or the timeout to expire.
+func Shutdown(
+	ctx context.Context,
+	timeout time.Duration,
+	ops map[string]Operation,
+	signals ...os.Signal,
+) <-chan struct{} {
+	if len(signals) == 0 {
+		signals = []os.Signal{syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP}
+	}
+
 	wait := make(chan struct{})
+
 	go func() {
 		s := make(chan os.Signal, 1)
-		signal.Notify(s, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+		signal.Notify(s, signals...)
 
 		select {
 		case sig := <-s:
-			slog.Info("shutting down due to signal", "signal", sig)
+			logger.Info("Shutting down due to signal", "signal", sig)
 		case <-ctx.Done():
-			slog.Info("shutting down due to context cancellation", "reason", ctx.Err())
+			logger.Info("Shutting down due to context cancellation", "reason", ctx.Err())
 		}
 
 		shutdownCtx, cancel := context.WithTimeout(context.Background(), timeout)
@@ -32,18 +60,20 @@ func Shutdown(ctx context.Context, timeout time.Duration, ops map[string]Operati
 		var wg sync.WaitGroup
 		for key, op := range ops {
 			wg.Add(1)
-			go func() {
+			go func(key string, op Operation) {
 				defer wg.Done()
-				slog.Info(fmt.Sprintf("cleaning up: %s", key))
+				logger.Info(fmt.Sprintf("Cleaning up: %s", key))
 				if err := op(shutdownCtx); err != nil {
-					slog.Info(fmt.Sprintf("%s: clean up failed: %s", key, err.Error()))
-					return
+					logger.Error("Cleanup failed", "component", key, "error", err)
+				} else {
+					logger.Info("Component shutdown successfully", "component", key)
 				}
-				slog.Info(fmt.Sprintf("%s was shutdown gracefully", key))
-			}()
+			}(key, op)
 		}
 		wg.Wait()
+
 		close(wait)
 	}()
+
 	return wait
 }
